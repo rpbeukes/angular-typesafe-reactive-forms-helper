10fcbf7cad4571ef7e34aebee1d98c47abb48693
Matt <matthowe82@gmail.com>
2020-03-18 17:33:22 +0800

    update imports

--- src/generateGetSafeFunction.ts
+++ src/generateGetSafeFunction.ts
@@ -1,3 +1,7 @@
+import { getPropertyName } from './getPropertyName';
+import { FormGroupTypeSafe } from './formGroupTypeSafe';
+
+
 export const generateGetSafeFunction = <T extends unknown>(gr: FormGroupTypeSafe<T>) => {
     return (propertyFunction: (typeVal: T) => any): AbstractControl => {
         const getStr = getPropertyName(propertyFunction.toString());
