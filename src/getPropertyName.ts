7a2e80f346512d4ccec6e220e27c73ffb7f972cf
Ruan Beukes <ruan.coder@gmail.com>
2020-03-18 21:21:19 +0800

    minor - npm run format

--- src/getPropertyName.ts
+++ src/getPropertyName.ts
@@ -1,29 +1,31 @@
 export const getPropertyName = (propertyFunction: string) => {
-    
-    let properties: string[] = [];
+  let properties: string[] = [];
 
-    if (propertyFunction) {
-        if (propertyFunction.toString().indexOf('=>') !== -1) {
-            // propertyFunction.toString() sample value:
-            // x => x.hero.address.postcode
-            // we need the 'hero.address.postcode'
-            // for gr.get('hero.address.postcode') function
-            properties = propertyFunction.split('=>')[1].trim()
-                                        .split('.')
-                                        .splice(1);
-        } else {
-            // https://github.com/dsherret/ts-nameof - helped me with the code below, THANX!!!!
-            // propertyFunction.toString() sample value:
-            //  function(x) { return x.hero.address.postcode;}
-            // we need the 'hero.address.postcode'
-            // for gr.get('hero.address.postcode') function
-            
-            // @ts-ignore (ignore the null complaint by typescript
-            properties = propertyFunction.match(/(return [;.a-zA-Z0-9 ]+)/gi)[0]
-                                        .match(/(?![. ])([a-z0-9_]+)(?=[};.])/gi).splice(1);
+  if (propertyFunction) {
+    if (propertyFunction.toString().indexOf('=>') !== -1) {
+      // propertyFunction.toString() sample value:
+      // x => x.hero.address.postcode
+      // we need the 'hero.address.postcode'
+      // for gr.get('hero.address.postcode') function
+      properties = propertyFunction
+        .split('=>')[1]
+        .trim()
+        .split('.')
+        .splice(1);
+    } else {
+      // https://github.com/dsherret/ts-nameof - helped me with the code below, THANX!!!!
+      // propertyFunction.toString() sample value:
+      //  function(x) { return x.hero.address.postcode;}
+      // we need the 'hero.address.postcode'
+      // for gr.get('hero.address.postcode') function
 
-        }
+      // @ts-ignore (ignore the null complaint by typescript
+      properties = propertyFunction
+        .match(/(return [;.a-zA-Z0-9 ]+)/gi)[0]
+        .match(/(?![. ])([a-z0-9_]+)(?=[};.])/gi)
+        .splice(1);
     }
+  }
 
-    return properties.join('.');
+  return properties.join('.');
 };
