e8aac9182659f1980976254803eb7ef44c935bfd
Matt <matthowe82@gmail.com>
2020-03-18 17:29:38 +0800

    move formGroupTypeSafe

---
+++ src/formGroupTypeSafe.ts
@@ -0,0 +1,11 @@
+import { FormGroup } from '@angular/forms';
+
+export abstract class FormGroupTypeSafe<T> extends FormGroup {
+    // give the value a custom type s
+    public value: T | undefined;
+
+    // create helper methods to achieve this syntax eg: this.form.getSafe(x => x.heroName).patchValue('Himan')
+    public abstract getSafe(propertyFunction: (typeVal: T) => any): AbstractControl;
+    public abstract setControlSafe(propertyFunction: (typeVal: T) => any, control: AbstractControl): void;
+    // If you need more function implement declare them here but implement them on FormBuilderTypeSafe.group instantiation.
+}
