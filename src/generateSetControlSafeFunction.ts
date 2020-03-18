8677d08cdb9f4156e53be3455391e60ed615033b
Matt <matthowe82@gmail.com>
2020-03-18 17:33:56 +0800

    update more imports

--- src/generateSetControlSafeFunction.ts
+++ src/generateSetControlSafeFunction.ts
@@ -1,3 +1,7 @@
+import { AbstractControl } from '@angular/forms';
+import { getPropertyName } from './getPropertyName';
+import { FormGroupTypeSafe } from './formGroupTypeSafe';
+
 export const generateSetControlSafeFunction = <T extends unknown>(gr: FormGroupTypeSafe<T>) => {
     return (propertyFunction: (typeVal: T) => any, control: AbstractControl): void => {
         const getStr = getPropertyName(propertyFunction.toString());
