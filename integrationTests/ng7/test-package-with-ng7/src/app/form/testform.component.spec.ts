a9dcc83f151b0eac9d11695b22980c517a2ea0eb
Ruan Beukes <ruan.coder@gmail.com>
2020-03-28 16:14:06 +0800

    -add test form with tests -add routing -remove app.component tests

---
+++ integrationTests/ng7/test-package-with-ng7/src/app/form/testform.component.spec.ts
@@ -0,0 +1,30 @@
+import { TestBed, async } from '@angular/core/testing';
+import { TestFormComponent } from './testform.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormBuilderTypeSafe } from 'angular-typesafe-reactive-forms-helper';
+describe('TestFormComponent', () => {
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [
+        TestFormComponent
+      ],
+      providers: [FormBuilderTypeSafe],
+      imports: [ ReactiveFormsModule ]
+    }).compileComponents();
+  }));
+
+  it('should use typesafe getHeroNameByValue', async(() => {
+    const fixture = TestBed.createComponent(TestFormComponent);
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('#getHeroNameByValue').textContent).toBe('He-Man');
+  }));
+
+  it('should use typesafe getHeroNameByGetSafe', async(() => {
+    const fixture = TestBed.createComponent(TestFormComponent);
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('#getHeroNameByGetSafe').textContent).toBe('He-Man');
+  }));
+
+});
---
+++ integrationTests/ng7/test-package-with-ng7/src/app/form/testform.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { /*FormGroup, FormBuilder,*/ Validators, FormControl } from '@angular/forms';
+import {FormBuilderTypeSafe, FormGroupTypeSafe} from 'angular-typesafe-reactive-forms-helper';
+
+interface HeroFormModel {
+  heroName: string;
+}
+
+@Component({
+  selector: 'app-test-form',
+  templateUrl: './testform.component.html',
+  styleUrls: ['./testform.component.css']
+})
+export class TestFormComponent implements OnInit {
+  title = 'test-package-with-ng7';
+
+  testForm: FormGroupTypeSafe<HeroFormModel>;
+
+  constructor(private fb: FormBuilderTypeSafe) { }
+
+  ngOnInit(): void {
+    this.testForm = this.fb.group<HeroFormModel>({
+      heroName: new FormControl('He-Man', Validators.required),
+    });
+  }
+
+  getHeroNameByValue = () => this.testForm.value.heroName;
+  getHeroNameByGetSafe = () => this.testForm.getSafe(x => x.heroName).value;
+
+}
