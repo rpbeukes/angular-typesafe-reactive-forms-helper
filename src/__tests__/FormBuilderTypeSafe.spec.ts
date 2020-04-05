b14e004c8ede5ef2cb2a73d9a0db5afeb556e5b9
Ruan Beukes <ruan.coder@gmail.com>
2020-04-05 13:23:05 +0800

    update tests with new typescript features

--- src/__tests__/FormBuilderTypeSafe.spec.ts
+++ src/__tests__/FormBuilderTypeSafe.spec.ts
@@ -1,9 +1,10 @@
-import { FormControl, Validators } from '@angular/forms';
+import { FormControl, Validators, FormArray } from '@angular/forms';
 import { FormBuilderTypeSafe, FormGroupTypeSafe } from '../angularTypesafeReactiveFormsHelper';
 
-// tslint:disable-next-line: interface-name
 interface TestContract {
   name: string;
+  lastName: string;
+  age: number;
 }
 
 describe(`When the FormBuilderTypeSafe initialises a group with FormBuilderTypeSafe.group<T>`, () => {
@@ -12,30 +13,32 @@ describe(`When the FormBuilderTypeSafe initialises a group with FormBuilderTypeS
   beforeEach(() => {
     const formBuilderTypeSafe = new FormBuilderTypeSafe();
     sut = formBuilderTypeSafe.group<TestContract>({
-      name: new FormControl('Hi-Man', Validators.required),
+      name: new FormControl('He-Man', Validators.required),
+      lastName: new FormControl('Grayskull', Validators.required),
+      age: new FormControl(88, Validators.required)
     });
   });
 
   test('the sut.getSafe(x => x.name) should return the correct value', () => {
-    expect(sut.getSafe(x => x.name).value).toBe('Hi-Man');
+    expect(sut?.getSafe(x => x.name)?.value).toBe('He-Man');
   });
 
   test('the type safe sut.value should return the correct value', () => {
-    expect(sut.value && sut.value.name).toBe('Hi-Man');
+    expect(sut?.value?.name).toBe('He-Man');
   });
 
   test('the sut.setControlSafe should set a new FormControl', () => {
     // set a new control with no validator
     sut.setControlSafe(x => x.name, new FormControl('Hulk'));
-    expect(sut.value && sut.value.name).toBe('Hulk');
-    expect(sut.getSafe(x => x.name).validator).toBeFalsy();
+    expect(sut?.value?.name).toBe('Hulk');
+    expect(sut?.getSafe(x => x.name)?.validator).toBeFalsy();
   });
 
   test('the sut.setValue should be typesafe and set the value', () => {
     // the test is basically...meh
     // if you want to test the typesafty, 
     // change the 'a new value' to 1, and the typescript compiler should shout at you.
-    sut.setValue({name: 'a new value'});
-    expect(sut.value && sut.value.name).toBe('a new value');
+    sut.setValue({name: 'a new value', age: 2, lastName : 'Grayskull'});
+    expect(sut?.value?.name).toBe('a new value');
   });
 });
