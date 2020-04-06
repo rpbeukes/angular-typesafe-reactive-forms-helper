7005cee0b5b1061845475ddf707b4df49be2ca72
Ruan Beukes <ruan.coder@gmail.com>
2020-04-06 20:28:31 +0800

    -add patch value test -add FormArray with a FormGroup object

--- src/__tests__/FormBuilderTypeSafe.spec.ts
+++ src/__tests__/FormBuilderTypeSafe.spec.ts
@@ -1,56 +1,70 @@
 import { FormControl, Validators, FormArray } from '@angular/forms';
 import { FormBuilderTypeSafe, FormGroupTypeSafe } from '../angularTypesafeReactiveFormsHelper';
 
-interface TestContract {
+interface HeroFormModel {
+  heroName: string;
+  weapons: WeaponModel[];
+}
+
+interface WeaponModel {
   name: string;
-  lastName: string;
-  age: number;
+  damagePoints: number;
 }
 
 describe(`When the FormBuilderTypeSafe initialises a group with FormBuilderTypeSafe.group<T>`, () => {
-  let sut: FormGroupTypeSafe<TestContract>; // system under test
+  let sut: FormGroupTypeSafe<HeroFormModel>; // system under test
 
   beforeEach(() => {
     const formBuilderTypeSafe = new FormBuilderTypeSafe();
-    sut = formBuilderTypeSafe.group<TestContract>({
-      name: new FormControl('He-Man', Validators.required),
-      lastName: new FormControl('Grayskull', Validators.required),
-      age: new FormControl(88, Validators.required)
+   
+    sut = formBuilderTypeSafe.group<HeroFormModel>({
+      heroName: new FormControl('He-Man', Validators.required),
+      weapons: new FormArray([formBuilderTypeSafe.group<WeaponModel>({
+            name: new FormControl('Sword', Validators.required),
+            damagePoints: new FormControl(50, Validators.required)
+        }),
+        formBuilderTypeSafe.group<WeaponModel>({
+            name: new FormControl('Shield', Validators.required),
+            damagePoints: new FormControl(0, Validators.required)
+        }),
+      ])
     });
+
   });
 
   test('the sut.getSafe(x => x.name) should return the correct value', () => {
-    expect(sut?.getSafe(x => x.name)?.value).toBe('He-Man');
+    expect(sut?.getSafe(x => x.heroName)?.value).toBe('He-Man');
   });
 
   test('the type safe sut.value should return the correct value', () => {
-    expect(sut?.value?.name).toBe('He-Man');
+    expect(sut?.value?.heroName).toBe('He-Man');
   });
 
   test('the sut.setControlSafe should set a new FormControl', () => {
     // set a new control with no validator
-    sut.setControlSafe(x => x.name, new FormControl('Hulk'));
-    expect(sut?.value?.name).toBe('Hulk');
-    expect(sut?.getSafe(x => x.name)?.validator).toBeFalsy();
+    sut.setControlSafe(x => x.heroName, new FormControl('Hulk'));
+    expect(sut?.value?.heroName).toBe('Hulk');
+    expect(sut?.getSafe(x => x.heroName)?.validator).toBeFalsy();
   });
 
   test('the sut.setValue should be typesafe and set the value', () => {
-    // the test is basically...meh
-    // if you want to test the typesafty, 
-    // change the 'a new value' to 1, and the typescript compiler should shout at you.
-    
-    // sut.setValue({name: 1, age: 2, lastName : 'Grayskull'}); // ERROR - Type 'number' is not assignable to type 'string'.
-    sut.setValue({name: 'a new value', age: 2, lastName : 'Grayskull'});
-    expect(sut?.value?.name).toBe('a new value');
+    sut.setValue({ heroName : 'Hulk', weapons: [
+      {name: 'Fist', damagePoints: 80 }, /* strict so one has to add exactly 2 elements into the array  */
+      {name: 'Head', damagePoints: 50 }
+    ]}, { onlySelf: true, emitEvent: true });
+
+    expect(sut.value.heroName).toEqual('Hulk');
+    expect(sut.value.weapons[0].name).toEqual('Fist');
+    expect(sut.value.weapons[0].damagePoints).toEqual(80);
   });
 
   test('the sut.patchValue should be typesafe and set partial value', () => {
-    // sut.patchValue({ name: 'BabyHulk', age: '2' }); // ERROR - Type 'string' is not assignable to type 'number | undefined'.
-    // sut.patchValue({ name: 'BabyHulk', age: 2, other: "bogus"  }); // ERROR - Argument of type '{ name: string; age: number; other: string; }' is not assignable to parameter of type 'Partial<TestContract>'
+    // sut.patchValue({ heroName: 1 }); //ERROR - Type 'number' is not assignable to type 'string | undefined'.
+    // sut.patchValue({ name: 'BabyHulk', age: 2, other: "bogus"  }); // ERROR - Argument of type '{ name: string; other: string; }' is not assignable to parameter of type 'Partial<TestContract>'
                                                                       //         Object literal may only specify known properties, and 'other' does not exist in type 'Partial<TestContract>'. 
-    sut.patchValue({ name: 'BabyHulk', age: 2 });
-    expect(sut?.value?.name).toBe('BabyHulk');
-    expect(sut?.value?.age).toBe(2);
-    expect(sut?.value?.lastName).toBe('Grayskull'); // un-changed
+    sut.patchValue({ heroName: 'BabyHulk' });
+    expect(sut?.value?.heroName).toBe('BabyHulk');
+    expect(sut?.value?.weapons[0].name).toBe('Sword');
+    expect(sut?.value?.weapons[1].name).toBe('Shield');
   });
 });
