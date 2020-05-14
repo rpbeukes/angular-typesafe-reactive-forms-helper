7cff4b777d7b7196b97038e12d23bd6e2928d740
Ruan Beukes <ruan.coder@gmail.com>
2020-05-14 19:44:54 +0800

    move test into 2 files

--- tests/__typeTests__/FormGroupTypeSafe.test.ts
+++ tests/__typeTests__/FormGroupTypeSafe.test.ts
@@ -2,76 +2,24 @@ import { createGroup } from '../createGroup';
 
 const sut = createGroup();
 
-const getSafeTests_AbstractControlTypeSafe = () => {
-    // $ExpectType AbstractControlTypeSafe<string> | null
-    sut.getSafe(x => x.heroName);
-    // $ExpectType AbstractControlTypeSafe<WeaponModel[]> | null
-    sut.getSafe(x => x.weapons);
-    // $ExpectType AbstractControlTypeSafe<string> | null
-    sut.getSafe(x => x.weapons[0].name);
-    // $ExpectType AbstractControlTypeSafe<number> | null
-    sut.getSafe(x => x.weapons[0].damagePoints);
-};
+const controlsTests_FormGroupTypeSafe = () => {
+  // $ExpectType string | undefined
+  sut.getSafe((x) => x.heroName)?.status;
 
-const getSafeTests_AbstractControlTypeSafe_value = () => {
-    // $ExpectType string | undefined
-    sut.getSafe(x => x.heroName)?.value;
-    // $ExpectType WeaponModel[] | undefined
-    sut.getSafe(x => x.weapons)?.value;
-};
-
-const getSafeTests_AbstractControlTypeSafe_valueChange = () => {
-    // ----- form.value changes --------
-    sut?.valueChanges.subscribe(val => {
-        // $ExpectType HeroFormModel
-        val;
-    });
-
-    // ----- specific property changes ------
-    sut.getSafe(x => x.heroName)?.valueChanges.subscribe(val => {
-        // $ExpectType string
-        val;
-    });
-
-    sut.getSafe(x => x.weapons)?.valueChanges.subscribe(val => {
-        // $ExpectType WeaponModel[]
-        val;
-    });
-
-    sut.getSafe(x => x.weapons)?.get([0])?.valueChanges.subscribe(val => {
-        // $ExpectType WeaponModel
-        val;
-    });
-
-    // the angular way - .get('person.name')
-    sut.getSafe(x => x.weapons)?.get('person.name')?.valueChanges.subscribe(val => {
-        // $ExpectType any
-        val;
-    });
-
-    // the angular way - .get(['person', 'name'])
-    sut.getSafe(x => x.weapons)?.get(['person', 'name'])?.valueChanges.subscribe(val => {
-        // $ExpectType any
-        val;
+  sut
+    .getSafe((x) => x.heroName)
+    ?.statusChanges.subscribe((val) => {
+      // $ExpectType ControlStatus
+      val;
     });
 };
 
 const statusChangesTests_FormGroupTypeSafe = () => {
-    // $ExpectType ControlStatus
-    sut.status;
+  // $ExpectType { heroName: AbstractControlTypeSafe<string>; weapons: AbstractControlTypeSafe<WeaponModel[]>; }
+  sut.controls;
 
-    sut.statusChanges.subscribe(val => {
-        // $ExpectType ControlStatus
-        val;
-    });
-};
-
-const statusChangesTests_AbstractControlTypeSafe = () => {
-    // $ExpectType string | undefined
-    sut.getSafe(x => x.heroName)?.status;
-
-    sut.getSafe(x => x.heroName)?.statusChanges.subscribe(val => {
-        // $ExpectType ControlStatus
-        val;
-    });
+  // $ExpectType AbstractControlTypeSafe<string>
+  sut.controls.heroName;
+  // $ExpectType AbstractControlTypeSafe<WeaponModel[]>
+  sut.controls.weapons;
 };
