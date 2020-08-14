910e1dfafa4e5cae482e5df4f87d3c4462dfd69f
Ruan Beukes <ruan.coder@gmail.com>
2020-08-15 00:09:34 +0800

    bump ng (#84)
    * bump ng
    
    * automate npm install too

---
+++ scriptUtilities/bump-ng-libraries-package-json.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+let packageJson = require('../package.json');
+const shelljs = require('shelljs');
+
+const main = () => {
+    const ngCurrentVersion = packageJson.devDependencies["@angular/common"];
+    const newVersion = process.argv && process.argv[2] || '';
+    if (newVersion) {
+        console.log(`current version: ${ngCurrentVersion}`);
+        console.log(`new version: ${newVersion}`);
+        
+        packageJson.devDependencies["@angular/common"] = newVersion;
+        packageJson.devDependencies["@angular/compiler"] = newVersion;
+        packageJson.devDependencies["@angular/core"] = newVersion;
+        packageJson.devDependencies["@angular/forms"] = newVersion;
+        packageJson.devDependencies["@angular/platform-browser"] = newVersion;
+
+        fs.writeFile('package.json', JSON.stringify(packageJson, null, 2), (err) => {
+            if (err) throw err;
+            console.log(`Success - check package.json`);
+
+            console.log(`> npm install`);
+            shelljs.exec(`npm install`).stdout;
+        });
+    } else {
+        console.log(`missing newVersion argument... 
+        eg: node ./scriptUtilities/bump-ng-libraries-package-json.js ~10.0.8`);
+    }
+};
+
+main();
