dcf25fa86a12f33fe763bc61638d2e19ee60a91c
Ruan Beukes <ruan.coder@gmail.com>
2020-11-03 21:46:07 +0800

    another go at e2e headless testing

--- integrationTests/ng10/test-package-with-ng10/e2e/src/app.e2e-spec.ts
+++ integrationTests/ng10/test-package-with-ng10/e2e/src/app.e2e-spec.ts
@@ -1,23 +1,39 @@
-import { AppPage } from './app.po';
-import { browser, logging } from 'protractor';
+import * as puppeteer from 'puppeteer';
 
 describe('End-to-End tests', () => {
-  let page: AppPage;
+  it('should render app-test-form', async () => {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
 
-  beforeEach(() => {
-    page = new AppPage();
-  });
+    await page.goto('http://localhost:4200');
 
-  it('should display page header "test-package-with-ngXX"', () => {
-    page.navigateTo();
-    expect(page.getTitleText()).toEqual('test-package-with-ng10');
-  });
-
-  afterEach(async () => {
-    // Assert that there are no errors emitted from the browser
-    const logs = await browser.manage().logs().get(logging.Type.BROWSER);
-    expect(logs).not.toContain(jasmine.objectContaining({
-      level: logging.Level.SEVERE,
-    } as logging.Entry));
+    const headerEl = await page.$('app-test-form h1');
+    const text = await page.evaluate(element => element.textContent, headerEl);
+    expect(text).toEqual('test-package-with-ng10');
+    await browser.close();
   });
 });
+
+// import { AppPage } from './app.po';
+// import { browser, logging } from 'protractor';
+
+// describe('End-to-End tests', () => {
+//   let page: AppPage;
+
+//   beforeEach(() => {
+//     page = new AppPage();
+//   });
+
+//   it('should display page header "test-package-with-ngXX"', () => {
+//     page.navigateTo();
+//     expect(page.getTitleText()).toEqual('test-package-with-ng10');
+//   });
+
+//   afterEach(async () => {
+//     // Assert that there are no errors emitted from the browser
+//     const logs = await browser.manage().logs().get(logging.Type.BROWSER);
+//     expect(logs).not.toContain(jasmine.objectContaining({
+//       level: logging.Level.SEVERE,
+//     } as logging.Entry));
+//   });
+// });
