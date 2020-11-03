4833910169d1f1fa8f4344b2c887e9a28c18c217
Ruan Beukes <ruan.coder@gmail.com>
2020-11-03 22:10:09 +0800

    remove comments from e2e test

--- integrationTests/ng10/test-package-with-ng10/e2e/src/app.e2e-spec.ts
+++ integrationTests/ng10/test-package-with-ng10/e2e/src/app.e2e-spec.ts
@@ -13,27 +13,3 @@ describe('End-to-End tests', () => {
     await browser.close();
   });
 });
-
-// import { AppPage } from './app.po';
-// import { browser, logging } from 'protractor';
-
-// describe('End-to-End tests', () => {
-//   let page: AppPage;
-
-//   beforeEach(() => {
-//     page = new AppPage();
-//   });
-
-//   it('should display page header "test-package-with-ngXX"', () => {
-//     page.navigateTo();
-//     expect(page.getTitleText()).toEqual('test-package-with-ng10');
-//   });
-
-//   afterEach(async () => {
-//     // Assert that there are no errors emitted from the browser
-//     const logs = await browser.manage().logs().get(logging.Type.BROWSER);
-//     expect(logs).not.toContain(jasmine.objectContaining({
-//       level: logging.Level.SEVERE,
-//     } as logging.Entry));
-//   });
-// });
