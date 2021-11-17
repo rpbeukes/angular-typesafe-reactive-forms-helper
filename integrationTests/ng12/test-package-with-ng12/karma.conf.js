820b26f81401c0f641a6ccb19f18a0af7a27b06e
Ruan Beukes <ruan.coder@gmail.com>
2021-11-17 19:48:32 +0800

    add ng12

--- integrationTests/ng12/test-package-with-ng12/karma.conf.js
+++ integrationTests/ng12/test-package-with-ng12/karma.conf.js
@@ -2,15 +2,15 @@
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
 module.exports = function (config) {
-  config.set({
+  const defaults = {
     basePath: '',
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
     plugins: [
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
-      require('karma-coverage'),
-      require('@angular-devkit/build-angular/plugins/karma')
+      require('karma-coverage-istanbul-reporter'),
+      require('@angular-devkit/build-angular/plugins/karma'),
     ],
     client: {
       jasmine: {
@@ -19,18 +19,15 @@ module.exports = function (config) {
         // for example, you can disable the random execution with `random: false`
         // or set a specific seed with `seed: 4321`
       },
-      clearContext: false // leave Jasmine Spec Runner output visible in browser
+      clearContext: false, // leave Jasmine Spec Runner output visible in browser
     },
     jasmineHtmlReporter: {
-      suppressAll: true // removes the duplicated traces
+      suppressAll: true, // removes the duplicated traces
     },
-    coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/test-package-with-ng12'),
-      subdir: '.',
-      reporters: [
-        { type: 'html' },
-        { type: 'text-summary' }
-      ]
+    coverageIstanbulReporter: {
+      dir: require('path').join(__dirname, './coverage/test-package-with-ng11'),
+      reports: ['html', 'lcovonly', 'text-summary'],
+      fixWebpackSourcePaths: true,
     },
     reporters: ['progress', 'kjhtml'],
     port: 9876,
@@ -39,6 +36,33 @@ module.exports = function (config) {
     autoWatch: true,
     browsers: ['Chrome'],
     singleRun: false,
-    restartOnFileChange: true
-  });
+    restartOnFileChange: true,
+  };
+
+  if (process.env.CI) {
+    Object.assign(defaults, {
+      autoWatch: false,
+      browsers: ['ChromeHeadlessNoSandbox'],
+      reporters: ['progress', 'kjhtml'],
+      singleRun: true,
+      customLaunchers: {
+        ChromeHeadlessNoSandbox: {
+          base: 'ChromeHeadless',
+          flags: [
+            '--no-sandbox',
+            '--headless',
+            '--disable-gpu',
+            '--disable-translate',
+            '--disable-web-security',
+            '--disable-extensions',
+            '--remote-debugging-port=9222',
+          ],
+          debug: false,
+        },
+      },
+      browserNoActivityTimeout: 30000,
+    });
+  }
+
+  config.set(defaults);
 };
