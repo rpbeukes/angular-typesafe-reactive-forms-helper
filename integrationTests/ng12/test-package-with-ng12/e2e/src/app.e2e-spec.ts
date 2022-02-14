c7451ff4509588a59a231a23d8b80c8357073336
Ruan Beukes <ruan.coder@gmail.com>
2022-02-14 16:53:07 +0800

    update the actual e2e test

--- integrationTests/ng12/test-package-with-ng12/e2e/src/app.e2e-spec.ts
+++ integrationTests/ng12/test-package-with-ng12/e2e/src/app.e2e-spec.ts
@@ -1,24 +1,19 @@
-import * as puppeteer from 'puppeteer';
+/// <reference types="cypress" />
 
 describe('End-to-End tests', () => {
   it('should render app-test-form', async () => {
+    console.log('process.env.NG_VERSION:', process.env.NG_VERSION);
+    
     process.env.NG_VERSION = process.env.NG_VERSION || 'process-env-VERSION-not-set';
-
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-
-    await page.goto('http://localhost:4200');
-
-    const headerEl = await page.$('app-test-form h1');
+    console.log('process.env.NG_VERSION:', process.env.NG_VERSION);
+    cy.visit('http://localhost:4200')
+    
+    const headerEl = cy.get('app-test-form h1');
 
     if (!headerEl) {
-      await browser.close();
       throw Error('Did not find \'app-test-form h1\'. More than likely the component did not render as expected :(');
     }
 
-    const text = await page.evaluate(element => element && element.textContent, headerEl);
-    expect(text).toEqual(`test-package-with-ng${process.env.NG_VERSION}`);
-
-    await browser.close(); 
+    headerEl.should('have.text', `test-package-with-${process.env.NG_VERSION}`);
   });
 });
